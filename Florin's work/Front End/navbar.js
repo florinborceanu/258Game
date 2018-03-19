f360f75b2041e79b15f3c2adef2a491c7f967f4e
florinborceanu <b.florin0405@gmail.com>
2018-03-20 00:53:04 +0200

    NavBar 2.0

--- Florin's work/Front End/navbar.js
+++ Florin's work/Front End/navbar.js
@@ -1,8 +1,21 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-} 
+'use strict'
+
+function showMenu(){
+    var menuEl = document.getElementsByClassName('icon')[0];
+var dropdownEl = document.getElementsByClassName('basic-navbar-buttons-wrapper')[0];
+var backdropEl = document.getElementsByClassName('backdrop')[0];
+dropdownEl.className = dropdownEl.className.replace('is-hidden', '');
+backdropEl.className = backdropEl.className.replace('is-hidden', '');
+    menuEl.className += " is-hidden";
+
+}
+
+function closeMenu(){
+    var menuEl = document.getElementsByClassName('icon')[0];
+var dropdownEl = document.getElementsByClassName('basic-navbar-buttons-wrapper')[0];
+var backdropEl = document.getElementsByClassName('backdrop')[0];
+    
+menuEl.className = menuEl.className.replace('is-hidden', '');
+    dropdownEl.className += " is-hidden";
+    backdropEl.className += " is-hidden";
+}
