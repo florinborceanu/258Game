b9797ed8178499d76dd9b5ff83b32734c1d59bbb
mititeluteofil <mititelu.teofil@yahoo.ro>
2018-03-20 05:49:07 +0200

    Team v.0.3b
    modified the pop-up and the paragraph
    -left to make 'em prettty

--- Teofil's work/Front End/scripts.js
+++ Teofil's work/Front End/scripts.js
@@ -16,24 +16,24 @@
    // popUp.classname = popUp.className.replace(" none", " active");
     popUp.style.display = "block"; 
     closure.style.display = "block"; 
-    if(n=1){
+    if(n ==1){
       index = 1;
       var user1=document.getElementsByClassName("user1")[0];
       user1.style.display = "block"; 
     }
-    if(n=2){
+    if(n ==2){
       index = 2;
       var user2=document.getElementsByClassName("user2")[0];
       user2.style.display = "block"; 
 
     }
-    if(n=3){
+    if(n ==3){
       index = 3;
       var user3=document.getElementsByClassName("user3")[0];
       user3.style.display = "block"; 
 
     }
-    if(n=4){
+    if(n ==4){
       index = 4;
       var user4=document.getElementsByClassName("user4")[0];
       user4.style.display = "block"; 
@@ -46,6 +46,25 @@
       popUp.classname = popUp.className.replace(" none", " active");
       popUp.style.display = "none"; 
       closure.style.display = "none"; 
+      if(index ==1){
+        var user1=document.getElementsByClassName("user1")[0];
+        user1.style.display = "none"; 
+      }
+      if(index ==2){
+        var user2=document.getElementsByClassName("user2")[0];
+        user2.style.display = "none"; 
+  
+      }
+      if(index ==3){
+        var user3=document.getElementsByClassName("user3")[0];
+        user3.style.display = "none"; 
+  
+      }
+      if(index ==4){
+        var user4=document.getElementsByClassName("user4")[0];
+        user4.style.display = "none"; 
+  
+      }
       
   }
   function showSlides(n) {
