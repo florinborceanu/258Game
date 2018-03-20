f2ee7ca0add66df0f2ad02275ec8708bf8978bba
mititeluteofil <mititelu.teofil@yahoo.ro>
2018-03-20 05:40:20 +0200

    Team v.0.3
    Pop-ups for images

--- Teofil's work/Front End/scripts.js
+++ Teofil's work/Front End/scripts.js
@@ -9,7 +9,45 @@
   function currentSlide(n) {
     showSlides(slideIndex = n);
   }
-  
+  var index = 0;
+  function popup(n){
+    var popUp = document.getElementsByClassName("popUpArt")[0];
+    var closure = document.getElementsByClassName("closure")[0];
+   // popUp.classname = popUp.className.replace(" none", " active");
+    popUp.style.display = "block"; 
+    closure.style.display = "block"; 
+    if(n=1){
+      index = 1;
+      var user1=document.getElementsByClassName("user1")[0];
+      user1.style.display = "block"; 
+    }
+    if(n=2){
+      index = 2;
+      var user2=document.getElementsByClassName("user2")[0];
+      user2.style.display = "block"; 
+
+    }
+    if(n=3){
+      index = 3;
+      var user3=document.getElementsByClassName("user3")[0];
+      user3.style.display = "block"; 
+
+    }
+    if(n=4){
+      index = 4;
+      var user4=document.getElementsByClassName("user4")[0];
+      user4.style.display = "block"; 
+
+    }
+  }
+  function closureFct(){
+      var popUp = document.getElementsByClassName("popUpArt")[0];
+      var closure = document.getElementsByClassName("closure")[0];
+      popUp.classname = popUp.className.replace(" none", " active");
+      popUp.style.display = "none"; 
+      closure.style.display = "none"; 
+      
+  }
   function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
