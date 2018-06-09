e02524bc6faa68c0c283d15ef90639c20639378e
Borceanu Florin <b.florin0405@gmail.com>
2018-06-09 14:41:05 +0300

    API WIP

--- _Project_MVC/App/Views/Home/Header.php
+++ _Project_MVC/App/Views/Home/Header.php
@@ -1,17 +1,3 @@
-<?php 
-  session_start(); 
-
-  if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: login.php");
-  }
-?>
-
 <html>
     <head>
         <style type="text/css">
